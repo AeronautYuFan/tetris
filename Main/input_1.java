7d811161aee292c22c515b742758b8cf7e31f0a5
AeronautAeronaut <105654510+AeronautAeronaut@users.noreply.github.com>
2022-06-01 09:54:21 -0400

    Add files via upload

---
+++ Main/input_1.java
@@ -0,0 +1,19 @@
+import java.awt.event.KeyAdapter;              
+import java.awt.event.KeyEvent;
+public class input_1 extends KeyAdapter
+{
+    public void keyPressed(KeyEvent e){                                          
+    // New key press
+    int key = e.getKeyCode();                                                
+    if (key == KeyEvent.VK_RIGHT) {                                          
+        //TO DO
+    } 
+    else if (key == KeyEvent.VK_LEFT) {                                    
+        //TO DO
+    }
+    if (key == KeyEvent.VK_DOWN) {
+        //TO DO
+    }
+    
+}
+}
