0d0a16bec4bc644e45519a63cf1d0248613e547b
JasonA123 <105654142+JasonA123@users.noreply.github.com>
2022-06-06 09:45:40 -0400

    Update Tetris.java

--- Main/Tetris.java
+++ Main/Tetris.java
@@ -3,10 +3,10 @@ import java.awt.*;
 
 public class Tetris extends JFrame {
     public Tetris(){
-        add(new gameBoard());
+        add(new gameBoard(), BorderLayout.WEST);
+        add(new scoreBoard(), BorderLayout.EAST);
         setDefaultCloseOperation (EXIT_ON_CLOSE);
         pack();
-
         setLocationRelativeTo(null);
         setResizable (true);
         setVisible (true);
