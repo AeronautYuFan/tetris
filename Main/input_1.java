a253e6f9e0ec84f5b796b0e93d3ebe545d586138
AeronautAeronaut <105654510+AeronautAeronaut@users.noreply.github.com>
2022-06-06 09:44:54 -0400

    Update input_1.java

--- Main/input_1.java
+++ Main/input_1.java
@@ -4,16 +4,20 @@ public class input_1 extends KeyAdapter
 {
     public void keyPressed(KeyEvent e){                                          
     // New key press
-    int key = e.getKeyCode();                                                
+    int key = e.getKeyCode();  
+    //right arrow key pressed
     if (key == KeyEvent.VK_RIGHT) {                                          
         //TO DO
-    } 
+    }
+    //left arrow key pressed
     else if (key == KeyEvent.VK_LEFT) {                                    
         //TO DO
     }
+    //down arrow key pressed
     if (key == KeyEvent.VK_DOWN) {
         //TO DO
     }
+    //up arrow key pressed
     if (key == KeyEvent.VK_UP) {
         //TO DO
     }
