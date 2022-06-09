efe14128b8cd35c106a3a67e858c4ad9b419e3e5
JasonA123 <105654142+JasonA123@users.noreply.github.com>
2022-06-09 10:05:46 -0400

    Update Block.java

--- Main/Block.java
+++ Main/Block.java
@@ -1,63 +1,32 @@
 import java.awt.*;
 
-class Block implements Yes {
-    private int xLoc;
-    private int yLoc;
+public class Block {
+    private Point origin;
+
+    private Color realColor;
+
+    public Block(int x, int y) {
+        origin = new Point(x, y);
+    }
+
+    public int getOriginX() { return origin.x; }
+
+    public int getOriginY() { return origin.y; }
+
+    public void setOriginX(int x) { origin.x = x; }
+
+    public void setOriginY(int y) { origin.y = y; }
+
+    public Color getColor() { return realColor; }
+
+    public void setColor(Color c) { realColor = c; }
+
+    public void drawPiece(Graphics g){
+        g.setColor(Color.RED);
+        g.fillRect(origin.x*Source.TILESIZE, origin.y*Source.TILESIZE,Source.TILESIZE-1, Source.TILESIZE-1);
+    }
     
-    public Block(int x) {
-        xLoc = x;
-        yLoc = 0;
-    }
-    
-    public void setY(int y) {
-        yLoc = y;
-    }
 
-    public int getX() {
-        return xLoc;
-    }
-
-    public int getY() {
-        return yLoc;
-    }
-
-    public void timedMoveDown() {
-        this.moveDown();
-    }
-
-    public void moveDown() {
-        yLoc++;
-    }
-
-    public boolean moveLeft() {
-       if (this.isGrounded() == false) {
-        if(xLoc > 0) {
-            xLoc--;
-            return true;
-        }
-       }
-        return false;
-    }
-
-    public boolean moveRight() {
-       if (this.isGrounded() == false) {
-        if(xLoc < 12) {
-            xLoc++;
-            return true;
-        }
-       }
-        return false;
-    }
-
-    public boolean isGrounded() {
-        // TO DO
-        return false;
-        // TO DO
-    }
-
-    public void removeBlock() {
-        // TO DO
-    }
 
 
 }
