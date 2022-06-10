76efa308251bc8a5d9a72ee4896e09ff8d9e5a60
JasonA123 <105654142+JasonA123@users.noreply.github.com>
2022-06-10 10:56:57 -0400

    Update scoreBoard.java

--- Main/scoreBoard.java
+++ Main/scoreBoard.java
@@ -1,21 +1,29 @@
 import java.awt.*;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class scoreBoard extends JPanel {
     // keeps track of the score of the player
-    private int score;
-    private int line;
-    private int level;
+
+    private Score realScore;
+    private JLabel scoreBox;
 
     public scoreBoard() {
+        realScore = new Score();
+        scoreBox = new JLabel("Score: " + realScore.getPoints());
+        //scoreBox.setVisible(true);
+
+        this.add(scoreBox);
+
         //sets the dimensions of the board
         setPreferredSize(new Dimension(200, 441));
         setBackground(Color.LIGHT_GRAY);
-
     }
 
     @Override // JTextField
     public void paintComponents(Graphics g) {
 
     }
+
+
+
 }
