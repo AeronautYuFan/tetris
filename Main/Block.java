ddbd4ea899f70384d14a5bb6f04493866f8163c9
AeronautAeronaut <105654510+AeronautAeronaut@users.noreply.github.com>
2022-06-10 10:28:10 -0400

    Update Block.java

--- Main/Block.java
+++ Main/Block.java
@@ -1,15 +1,15 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
-public class Block extends JPanel {
+public class Block extends JPanel{
     private Point origin;
-
     private Color realColor;
-
-    public Block(int x, int y) {
-        origin = new Point(x, y);
+    public Block() {
+        origin = new Point(6, 0);
         realColor = Color.RED;
-        Source.setFillValue(y, x, true);
-
     }
 
     public int getOriginX() { return origin.x; }
@@ -23,6 +23,27 @@ public class Block extends JPanel {
     public Color getRealColor() { return realColor; }
 
     public void setRealColor(Color c) { realColor = c; }
-    
+
+    public void drawPiece(Graphics g){
+        g.setColor(Color.RED);
+        g.fillRect(origin.x*Source.TILESIZE, origin.y*Source.TILESIZE,Source.TILESIZE-1, Source.TILESIZE-1);
+    }
+
+    public void moveDown(){
+        origin.y++;
+        repaint();
+    }
+
+    public void moveRight(){
+        origin.x++;
+        repaint();
+    }
+
+    public void moveLeft(){
+        origin.x--;
+        repaint();
+    }
+
+
 
 }
