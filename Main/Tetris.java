e677d0ebbbbf9a36c037974e9b128be3ff93e95c
AeronautYuFan <57840445+AeronautYuFan@users.noreply.github.com>
2022-06-10 12:26:21 -0400

    attempted to add restart button (incomplete)

--- Main/Tetris.java
+++ Main/Tetris.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event;
 
 public class Tetris extends JFrame {
     private gameBoard board;
     private scoreBoard sBoard;
+    private JButton bigRedButton;
+    
     public Tetris(){
         board = new gameBoard();
         sBoard = new scoreBoard();
+        bigRedButton = new JButton("Restart");
+        bigRedButton.addActionListener(new restartListener() );
 
         add(board, BorderLayout.WEST);
         add(sBoard, BorderLayout.EAST);
@@ -16,11 +21,29 @@ public class Tetris extends JFrame {
         setResizable (true);
         setVisible (true);
     }
+    
+    private class restartListener implements ActionListener {
+        public void actionPerformed (ActionEvent event) {
+            refresh();
+        }
+    }
 
     public static void main(String[] args) {
         new Tetris();
     }
 
-
-
+    public void refresh() {
+        board = new gameBoard();
+        sBoard = new scoreBoard();
+        Source.restart();
+        
+        add(board, BorderLayout.WEST);
+        add(sBoard, BorderLayout.EAST);
+        setDefaultCloseOperation (EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable (true);
+        setVisible (true);
+    }
+    
 }
