76bd3a606ba6d93bdb7d696c08814722a1438bed
AeronautAeronaut <105654510+AeronautAeronaut@users.noreply.github.com>
2022-06-10 10:28:43 -0400

    Update Source.java

--- Main/Source.java
+++ Main/Source.java
@@ -5,7 +5,7 @@ public class Source {
     // # of columns
     public static int ROWS = 21;
 
-    public static boolean[][] FILL;
+    public static boolean[][] FILL = new boolean[ROWS][COLUMNS];
 
     public static void setFillValue(int row, int col, boolean f) {
         FILL[row][col] = f;
