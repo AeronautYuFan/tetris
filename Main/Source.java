1fee9fb534935cb86e00c282d40acb34dec4c2cd
AeronautYuFan <57840445+AeronautYuFan@users.noreply.github.com>
2022-06-12 12:59:45 -0400

    Update Source.java

--- Main/Source.java
+++ Main/Source.java
@@ -5,7 +5,7 @@ public class Source {
     // # of columns
     public static int ROWS = 21;
 
-    public static boolean[][] FILL = new boolean[ROWS][COLUMNS];
+    public static boolean[][] FILL = new boolean[ROWS - 1][COLUMNS - 2];
     
     public static int SCORE = 0;
     
@@ -15,10 +15,16 @@ public class Source {
     public static void setFillValue(int row, int col, boolean f) {
         FILL[row][col] = f;
     }
+
+    public static void setFillValue(int row, boolean f) { FILL[row] = new boolean[ROWS]; }
     
     public static void restart() {
         FILL = new boolean[ROWS][COLUMNS];
         SCORE = 0;
         CLEAREDROWS = 0;
+    }
+
+    public static void addSCORE(int x) {
+        SCORE += x;
     }
 }
