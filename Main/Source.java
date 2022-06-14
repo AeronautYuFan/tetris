64c6e577e5ff9236a7736cf438adbf42194e098a
JasonA123 <105654142+JasonA123@users.noreply.github.com>
2022-06-14 19:09:23 -0400

    Update Source.java

--- Main/Source.java
+++ Main/Source.java
@@ -6,11 +6,11 @@ public class Source {
     public static int ROWS = 21;
 
     public static boolean[][] FILL = new boolean[COLUMNS - 2][ROWS - 1];
-    
+
     public static int SCORE = 0;
-    
+
     public static int CLEAREDROWS = 0;
-    
+
 
     public static void setFillValue(int col, int row, boolean f) { FILL[col][row] = f; }
 
@@ -19,7 +19,7 @@ public class Source {
             FILL[c][realRow] = f;
         }
     }
-    
+
     public static void restart() {
         FILL = new boolean[COLUMNS - 2][ROWS - 1];
         SCORE = 0;
