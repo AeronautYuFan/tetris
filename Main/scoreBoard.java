78db6114129805b29549514e19858f736a9ddeef
AeronautYuFan <57840445+AeronautYuFan@users.noreply.github.com>
2022-06-19 12:50:36 -0400

    added game over screen

--- Main/scoreBoard.java
+++ Main/scoreBoard.java
@@ -5,10 +5,19 @@ public class scoreBoard extends JPanel {
     // keeps track of the score of the player
 
     private JLabel scoreBox;
+    private JLabel gameOver;
+
+    //private
 
     public scoreBoard() {
         scoreBox = new JLabel("Score: " + Source.SCORE);
         this.add(scoreBox);
+        gameOver = new JLabel("Game over!");
+        gameOver.setLocation(100, 100);
+        gameOver.setForeground(Color.RED);
+        this.add(gameOver);
+        gameOver.setVisible(false);
+
         //sets the dimensions of the board
         setPreferredSize(new Dimension(200, 441));
         setBackground(Color.LIGHT_GRAY);
@@ -16,5 +25,9 @@ public class scoreBoard extends JPanel {
 
     public void updateScore(){
         scoreBox.setText("Score: " + Source.SCORE);
+    }
+
+    public void showGameOver() {
+        gameOver.setVisible(true);
     }
 }
